d60ceeba9e638adad6a69e0310b116ff3fa63354
dave@nonno.tv <dn@Davids-MacBook-Air.local>
2014-11-12 17:48:47 -0500

    More formatting fun.

--- vectorExample.R
+++ vectorExample.R
@@ -1,10 +1,8 @@
-## In this example we introduce the <<- operator which can be used to assign a value 
-## to an object in an environment that is different from the current environment. 
-## Below are two functions that are used to create a special object that stores a 
-## numeric vector and caches its mean.
+## Below are two functions that are used to create a 
+## special object that stores a numeric vector and caches its mean.
 ## 
-## The first function, makeVector creates a special "vector", which is really a 
-## list containing a function to
+## The first function, makeVector creates a special "vector", 
+## which is really a list containing a function to:
 ## 
 ## 1. set the value of the vector
 ## 2. get the value of the vector
